037a3c6ad4dd10dd4b079299020c64ea0dbc8c1d
Walt <techquasar@gmail.com>
2017-12-25 20:02:04 +0000

    added modt and apche default

---
+++ cookbooks/Quetzal/README.md
@@ -0,0 +1,4 @@
+# Quetzal
+
+TODO: Enter the cookbook description here.
+
--- cookbooks/Quetzal/metadata.rb
+++ cookbooks/Quetzal/metadata.rb
@@ -1,20 +1,20 @@
-name 'apache'
+name 'Quetzal'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
-description 'Installs/Configures apache'
-long_description 'Installs/Configures apache'
-version '0.2.1'
+description 'Installs/Configures Quetzal'
+long_description 'Installs/Configures Quetzal'
+version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/apache/issues'
+# issues_url 'https://github.com/<insert_org_here>/Quetzal/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/apache'
+# source_url 'https://github.com/<insert_org_here>/Quetzal'
