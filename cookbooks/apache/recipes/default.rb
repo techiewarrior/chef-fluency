b12557ca9f873081c6f4e5c4bdf4390667fc21c9
Walt <techquasar@gmail.com>
2017-12-25 22:12:10 +0000

    added postgresql cookbook

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,10 +4,10 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-if node['platform-family'] == 'rhel'
-       package = 'httpd'
-elseif node ['platform_family'] == 'debian'
-       package = 'apache2'
+if node['platform_family'] == "rhel"
+       package = "httpd"
+elsif node['platform_family'] == "debian"
+       package = "apache2"
 end
 
 
--- cookbooks/apache/recipes/motd.rb
+++ cookbooks/apache/recipes/motd.rb
@@ -1,4 +1,4 @@
-hostname = node ['hostname']
+hostname = node['hostname']
 file '/etc/motd' do
 	content "Hostname is this: #{hostname}"
 end
